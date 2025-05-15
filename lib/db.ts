51be2a492b5b7c0e10a601db1d7e4642839c4745
Omar Kraidié <omppula_1@hotmail.com>
2025-05-15 15:47:51 +0300

    Change let to var for prisma declaration in db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from '@/generated/prisma/client';
 
 declare global {
-	let prisma: PrismaClient | undefined;
+	var prisma: PrismaClient | undefined;
 }
 
 const client = globalThis.prisma || new PrismaClient();
